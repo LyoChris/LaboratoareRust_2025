ebf76a80e7418ab5642af483fb58e74961d46ebf
LyoChris <cosminpatranea@gmail.com>
2025-12-11 02:30:54 +0200

    Adaugat o prima versiune pentru UI. Include un tabel cu procesele si informatii despre ele

--- task_manager/src/backend/mod.rs
+++ task_manager/src/backend/mod.rs
@@ -1,4 +1,4 @@
-use sysinfo::{ProcessesToUpdate, ProcessRefreshKind, System, UpdateKind, Users};
+use sysinfo::{ProcessRefreshKind, ProcessesToUpdate, System, UpdateKind, Users};
 //use::std::{thread, thread::sleep};
 //use::std::time::Duration;
 
@@ -7,7 +7,7 @@ pub struct ProcessInfo {
     pub parent_pid: u32,
     pub name: String,
     pub cpu: f32,
-    pub memory: u64,
+    pub memory: f64,
     pub exe: String,
     pub user: String,
 }
@@ -25,16 +25,19 @@ pub trait InfoGetter {
 impl InfoGetter for Monitor {
     fn new() -> Self {
         let mut sys = System::new_all();
-        sys.refresh_processes_specifics(ProcessesToUpdate::All, true, ProcessRefreshKind::nothing()
-                                                                                        .with_cpu()
-                                                                                        .with_user(UpdateKind::OnlyIfNotSet)
-                                                                                        .with_memory()
-                                                                                        .with_exe(UpdateKind::OnlyIfNotSet),
+        sys.refresh_processes_specifics(
+            ProcessesToUpdate::All,
+            true,
+            ProcessRefreshKind::nothing()
+                .with_cpu()
+                .with_user(UpdateKind::OnlyIfNotSet)
+                .with_memory()
+                .with_exe(UpdateKind::OnlyIfNotSet),
         );
 
         let users = Users::new_with_refreshed_list();
 
-        Self {sys, users}
+        Self { sys, users }
     }
 
     fn system_info_update(&mut self) -> Vec<ProcessInfo> {
@@ -42,13 +45,13 @@ impl InfoGetter for Monitor {
             ProcessesToUpdate::All,
             true,
             ProcessRefreshKind::nothing()
-                            .with_cpu()        
-                            .with_user(UpdateKind::OnlyIfNotSet)
-                            .with_memory()
-                            .with_exe(UpdateKind::OnlyIfNotSet),
+                .with_cpu()
+                .with_user(UpdateKind::OnlyIfNotSet)
+                .with_memory()
+                .with_exe(UpdateKind::OnlyIfNotSet),
         );
 
-        let mut process_info : Vec<ProcessInfo> = Vec::new();
+        let mut process_info: Vec<ProcessInfo> = Vec::new();
 
         for (pid, process) in self.sys.processes() {
             let mut info = ProcessInfo {
@@ -56,24 +59,21 @@ impl InfoGetter for Monitor {
                 parent_pid: 0u32,
                 name: String::new(),
                 cpu: process.cpu_usage() / (self.sys.cpus().len() as f32),
-                memory: process.memory(),
+                memory: (process.memory() as f64) / 1024.0 / 1024.0,
                 exe: String::new(),
                 user: "Unknown".to_string(),
             };
 
             info.name = if let Some(correct_name) = process.name().to_str() {
                 correct_name.to_string()
-            }
-            else {
+            } else {
                 "Unknown".to_string()
             };
 
             match process.exe() {
-                Some(path) => {
-                    match path.to_str() {
-                        Some(correct_path) => info.exe = correct_path.to_string(),
-                        None => info.exe = "Unknown".to_string(),
-                    }
+                Some(path) => match path.to_str() {
+                    Some(correct_path) => info.exe = correct_path.to_string(),
+                    None => info.exe = "Unknown".to_string(),
                 },
                 None => info.exe = "Unknown".to_string(),
             }
@@ -82,9 +82,9 @@ impl InfoGetter for Monitor {
                 'lop: for user in self.users.list() {
                     if uid == user.id() {
                         info.user = user.name().to_string();
-                         break 'lop;
+                        break 'lop;
                     }
-                };
+                }
             }
 
             match process.parent() {
--- task_manager/src/main.rs
+++ task_manager/src/main.rs
@@ -1,18 +1,131 @@
-
 mod backend;
-use backend::{Monitor, InfoGetter};
+use backend::{Monitor, ProcessInfo, InfoGetter};
+use eframe::egui::{CentralPanel, Context, FontFamily, FontId, TextStyle};
+use egui_extras::{Column, TableBuilder};
 
-fn main() {
-    let mut moni: Monitor = Monitor::new();
+struct TaskManager {
+    monitor: Monitor,
+    processes: Vec<ProcessInfo>,
+}
 
-    for process in moni.system_info_update() {
-        println!("PID: {} | Parent PID: {} | Name: {} | CPU: {:.2}% | Memory: {} bytes | EXE: {} | User: {}", 
-                 process.pid, 
-                 process.parent_pid, 
-                 process.name, 
-                 process.cpu, 
-                 process.memory, 
-                 process.exe, 
-                 process.user);
-    }   
+impl Default for TaskManager {
+    fn default() -> Self {
+        let mut monitor = Monitor::new();
+        let processes = monitor.system_info_update();
+
+        Self { monitor, processes }
+    }
+}
+
+impl eframe::App for TaskManager {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        self.processes = self.monitor.system_info_update();
+        set_styles(ctx);
+        CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello from aplication");
+            ui.separator();
+
+            let width = ui.available_width();
+
+            TableBuilder::new(ui)
+                .vscroll(true)
+                .column(Column::initial(width * 0.2).resizable(true))
+                .column(Column::initial(width * 0.1).resizable(true))
+                .column(Column::initial(width * 0.1).resizable(true))
+                .column(Column::initial(width * 0.3).resizable(true))
+                .column(Column::initial(width * 0.2).resizable(true))
+                .header(20.0, |mut header| {
+                    header.col(|ui| {
+                        ui.heading("Process Name");
+                        ui.separator();
+                    });
+                    header.col(|ui| {
+                        ui.heading("CPU usage");
+                        ui.separator();
+                    });
+                    header.col(|ui| {
+                        ui.heading("Memory usage");
+                        ui.separator();
+                    });
+                    header.col(|ui| {
+                        ui.heading("Path");
+                        ui.separator();
+                    });
+                    header.col(|ui| {
+                        ui.heading("Username");
+                        ui.separator();
+                    });
+                })
+                .body(|body| {
+                    let height = 50.0;
+                    let num = self.processes.len();
+
+                    body.rows(height, num, |mut row| {
+                        let index = row.index();
+                        let process = &self.processes[index];
+
+                        row.col(|ui| {
+                            ui.label(format!("{}", process.name));
+                        });
+
+                        // Col 2: CPU
+                        row.col(|ui| {
+                            ui.label(format!("{}%", process.cpu));
+                        });
+
+                        // Col 3: Memory
+                        row.col(|ui| {
+                            ui.label(format!("{:.1} MB", process.memory));
+                        });
+
+                        // Col 4: Path
+                        row.col(|ui| {
+                            ui.label(format!("{}", process.exe));
+                        });
+
+                        // Col 5: Username
+                        row.col(|ui| {
+                            ui.label(format!("{}", process.user));
+                        });
+                    });
+                });
+        });
+    }
+}
+
+fn set_styles(ctx: &Context) {
+    let mut style = (*ctx.style()).clone();
+    style
+        .text_styles
+        .insert(TextStyle::Heading, FontId::new(30.0, FontFamily::Monospace));
+
+    ctx.set_style(style);
+}
+
+fn main() -> Result<(), eframe::Error> {
+    // let mut moni: Monitor = Monitor::new();
+
+    let options = eframe::NativeOptions {
+        viewport: eframe::egui::ViewportBuilder::default()
+            .with_resizable(true)
+            .with_inner_size([1000.0, 600.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "AICI",
+        options,
+        Box::new(|_cc| Ok(Box::<TaskManager>::default())),
+    )
+
+    // for process in moni.system_info_update() {
+    //     println!("PID: {} | Parent PID: {} | Name: {} | CPU: {:.2}% | Memory: {} bytes | EXE: {} | User: {}",
+    //              process.pid,
+    //              process.parent_pid,
+    //              process.name,
+    //              process.cpu,
+    //              process.memory,
+    //              process.exe,
+    //              process.user);
+    // }
 }
